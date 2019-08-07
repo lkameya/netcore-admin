c23f53b9b3ca6cd4ad8816f1d3895ee6718c9293
Leonardo Kameya <leonardo.kameya@gmail.com>
2019-08-07 17:34:03 -0400

    Installing and configuring Swagger for API management

---
+++ Options/SwaggerOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace netcore_admin.Options
+{
+    public class SwaggerOptions
+    {
+        public string JsonRoute { get; set; }
+
+        public string Description { get; set; }
+        public string UIEndpoint { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using netcore_admin.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using netcore_admin.Options;
 
 namespace netcore_admin
 {
@@ -21,13 +22,18 @@ namespace netcore_admin
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.AddMvc(options => { options.EnableEndpointRouting = false; }).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo{ Title = "Admin API", Version = "v1" });
+            });
             //services.AddControllersWithViews();
             //services.AddRazorPages();
         }
@@ -45,14 +51,22 @@ namespace netcore_admin
             }
 
             app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
+            app.UseStaticFiles();
+
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                option.RouteTemplate = swaggerOptions.JsonRoute;
             });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
+            });
+
+            app.UseMvc();
         }
     }
 }
